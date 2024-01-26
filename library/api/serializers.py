8dd98d3884872c1e183881fac64324f5c8af348b
kupidon06 <154974982+kupidon06@users.noreply.github.com>
2024-01-26 17:12:35 +0000

    Update serializers.py

--- library/api/serializers.py
+++ library/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Livre, Auteur, Emprunt
+from django.contrib.auth.models import User
 
 class LivreSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,3 +15,10 @@ class EmpruntSerializer(serializers.ModelSerializer):
     class Meta:
         model=Emprunt
         fields="__all__"
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields=['id','username','password','email']
+        extra_kwargs={'password':{'write_only':True}}
+
