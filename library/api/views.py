4f0203cbab76573fd27f7632d8b2de10a9b70218
kupidon06 <154974982+kupidon06@users.noreply.github.com>
2024-01-26 17:11:48 +0000

    Update views.py

--- library/api/views.py
+++ library/api/views.py
@@ -3,8 +3,33 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Livre, Auteur, Emprunt
-from .serializers import LivreSerializer, AuteurSerializer, EmpruntSerializer
+from .serializers import LivreSerializer, AuteurSerializer, EmpruntSerializer,UserSerializer
+from django.contrib.auth.models import User
 
+
+class Userdetails(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Détails d'un utilsateur
+
+    Cette vue gère les requêtes GET pour un auteur spécifique, permettant de récupérer ses détails.
+    Elle gère également les requêtes PUT et DELETE pour mettre à jour ou supprimer un auteur.
+    L'identifiant de l'auteur est fourni dans l'URL.
+    """
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = User.objects.all()
+
+class Userlist(generics.ListCreateAPIView):
+    """
+    Liste des utilisateurs
+
+    Cette vue gère les requêtes GET et POST pour les auteurs.
+    GET retourne une liste de tous les auteurs.
+    POST permet de créer un nouvel auteur avec les données fournies dans le corps de la requête.
+    """
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = User.objects.all()
 class Auteurdetails(generics.RetrieveUpdateDestroyAPIView):
     """
     Détails d'un auteur
@@ -31,10 +56,10 @@ class Auteurlist(generics.ListCreateAPIView):
 
 class Livredetails(generics.RetrieveUpdateDestroyAPIView):
     """
-    Détails d'un livre
+    Détails d'un user
 
-    Cette vue gère les requêtes GET pour un livre spécifique, permettant de récupérer ses détails.
-    Elle gère également les requêtes PUT et DELETE pour mettre à jour ou supprimer un livre.
+    Cette vue gère les requêtes GET pour un user spécifique, permettant de récupérer ses détails.
+    Elle gère également les requêtes PUT et DELETE pour mettre à jour ou supprimer un user.
     L'identifiant du livre est fourni dans l'URL.
     """
     serializer_class = LivreSerializer
@@ -43,12 +68,119 @@ class Livredetails(generics.RetrieveUpdateDestroyAPIView):
 
 class Livrelist(generics.ListCreateAPIView):
     """
-    Liste des livres
+    user des users
 
     Cette vue gère les requêtes GET et POST pour les livres.
-    GET retourne une liste de tous les livres avec la possibilité de filtrer par date de publication et genre.
-    POST permet de créer un nouveau livre avec les données fournies dans le corps de la requête.
+    GET retourne une user de tous les user avec la possibilité de filtrer par date de publication et genre.
+    POST permet de créer un nouveau user avec les données fournies dans le corps de la requête.
     """
+
+    serializer_class = LivreSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = Livre.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['date_publication', 'genre']
+
+class Emprunteurdetails(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Détails d'un emprunt
+
+    Cette vue gère les requêtes GET pour un emprunt spécifique, permettant de récupérer ses détails.
+    Elle gère également les requêtes PUT et DELETE pour mettre à jour ou supprimer un emprunt.
+    L'identifiant de l'emprunt est fourni dans l'URL.
+    """
+    serializer_class = EmpruntSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = Emprunt.objects.all()
+
+class Emprunteurlist(generics.ListCreateAPIView):
+    """
+    Liste des emprunts
+
+    Cette vue gère les requêtes GET et POST pour les emprunts.
+    GET retourne une liste de tous les emprunts.
+    POST permet de créer un nouvel emprunt avec les données fournies dans le corps de la requête.
+    """
+    serializer_class = EmpruntSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = Emprunt.objects.all()
+from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Livre, Auteur, Emprunt
+from .serializers import LivreSerializer, AuteurSerializer, EmpruntSerializer,UserSerializer
+from django.contrib.auth.models import User
+
+
+class Userdetails(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Détails d'un utilsateur
+
+    Cette vue gère les requêtes GET pour un auteur spécifique, permettant de récupérer ses détails.
+    Elle gère également les requêtes PUT et DELETE pour mettre à jour ou supprimer un auteur.
+    L'identifiant de l'auteur est fourni dans l'URL.
+    """
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = User.objects.all()
+
+class Userlist(generics.ListCreateAPIView):
+    """
+    Liste des utilisateurs
+
+    Cette vue gère les requêtes GET et POST pour les auteurs.
+    GET retourne une liste de tous les auteurs.
+    POST permet de créer un nouvel auteur avec les données fournies dans le corps de la requête.
+    """
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = User.objects.all()
+class Auteurdetails(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Détails d'un auteur
+
+    Cette vue gère les requêtes GET pour un auteur spécifique, permettant de récupérer ses détails.
+    Elle gère également les requêtes PUT et DELETE pour mettre à jour ou supprimer un auteur.
+    L'identifiant de l'auteur est fourni dans l'URL.
+    """
+    serializer_class = AuteurSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = Auteur.objects.all()
+
+class Auteurlist(generics.ListCreateAPIView):
+    """
+    Liste des auteurs
+
+    Cette vue gère les requêtes GET et POST pour les auteurs.
+    GET retourne une liste de tous les auteurs.
+    POST permet de créer un nouvel auteur avec les données fournies dans le corps de la requête.
+    """
+    serializer_class = AuteurSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = Auteur.objects.all()
+
+class Livredetails(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Détails d'un user
+
+    Cette vue gère les requêtes GET pour un user spécifique, permettant de récupérer ses détails.
+    Elle gère également les requêtes PUT et DELETE pour mettre à jour ou supprimer un user.
+    L'identifiant du livre est fourni dans l'URL.
+    """
+    serializer_class = LivreSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = Livre.objects.all()
+
+class Livrelist(generics.ListCreateAPIView):
+    """
+    user des users
+
+    Cette vue gère les requêtes GET et POST pour les livres.
+    GET retourne une user de tous les user avec la possibilité de filtrer par date de publication et genre.
+    POST permet de créer un nouveau user avec les données fournies dans le corps de la requête.
+    """
+
     serializer_class = LivreSerializer
     permission_classes = [IsAuthenticated]
     queryset = Livre.objects.all()
